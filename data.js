b36c1a810904727017720e75b3ca290ee87c14a8
Tran Minh Thuan <125931125+thuan1041@users.noreply.github.com>
2024-03-17 15:20:17 +0700

    Add files via upload
    lab 4 on gk local

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = [{
+    'id': 1,
+    'name': "Lập trình hướng đối tượng",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+    }, {
+    id: 2,
+    name: "Cơ sở dữ liệu",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+    }, {
+    id: 3,
+    name: "Lập trình web",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+    }, {
+    id: 4,
+    name: "Lập trình di động",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+    }, {
+    id: 5,
+    name: "An toàn và bảo mật hệ thống",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+    }, {
+    id: 6,
+    name: "Phân tích và thiết kế hệ thống",
+    course_type: "Công nghệ thông tin",
+    semester: "Kì 1",
+    department: "Công nghệ thông tin",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Tomato.png"
+}]
+
+// export default data;
+module.exports = data
